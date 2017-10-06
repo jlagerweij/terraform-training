27777721fca74b15aa03a077e24d2305e867e151
HashiCorp Training <training@hashicorp.com>
2017-10-06 13:19:44 +0000

    Updates from training

---
+++ main.tf
@@ -0,0 +1,66 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-5a922335
+#
+# Your subnet ID is:
+#
+#     subnet-faec2e91
+#
+# Your security group ID is:
+#
+#     sg-c7eb27ad
+#
+# Your Identity is:
+#
+#     asas-ostrich
+#
+
+terraform {
+  backend "atlas" {
+    name = "jlagerweij/training"
+  }
+}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
+variable "num_webs" {
+  default = "2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-5a922335"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-faec2e91"
+  vpc_security_group_ids = ["sg-c7eb27ad"]
+  count                  = "${var.num_webs}"
+
+  tags {
+    "Identity" = "asas-ostrich"
+    "Trainee"  = "Jos"
+    "Date"     = "6 october"
+    "Name"     = "web ${count.index + 1} / ${var.num_webs}"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
